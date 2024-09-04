462ccbbe5894686174a83b8eebc4a2d208b57214
adrianjankowicz <adrianj82@gmail.com>
2024-09-05 01:21:01 +0200

    added colors to appointments and translates

---
+++ src/components/BasicLayout.tsx
@@ -0,0 +1,36 @@
+import { AppointmentForm } from "@devexpress/dx-react-scheduler-material-ui";
+import { FormControl, Grid2 as Grid, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+
+
+const BasicLayout = ({ onFieldChange, appointmentData, ...restProps }: any) => {
+  const handleTypeChange = (event: SelectChangeEvent) => {
+    onFieldChange({ type: event.target.value });
+  };
+
+  return (
+    <AppointmentForm.BasicLayout
+      appointmentData={appointmentData}
+      onFieldChange={onFieldChange}
+      {...restProps}
+    >
+      <Grid container spacing={2} className='mt-4'>
+        <Grid size={12}>
+          <FormControl fullWidth>
+            <InputLabel>Rodzaj Spotkania</InputLabel>
+            <Select
+              value={appointmentData.type || "Osobiste"}
+              onChange={handleTypeChange}
+              label="Rodzaj Spotkania"
+            >
+              <MenuItem value={"Meeting"}>Spotkanie</MenuItem>
+              <MenuItem value={"Work"}>Praca</MenuItem>
+              <MenuItem value={"Personal"}>Osobiste</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    </AppointmentForm.BasicLayout>
+  );
+};
+
+export default BasicLayout;
---
+++ src/types/types.tsx
@@ -0,0 +1,22 @@
+import { User } from "firebase/auth";
+
+export interface AppointmentModel {
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  rRule?: string;
+  exDate?: string;
+  allDay: boolean;
+  location?: string;
+  notes?: string;
+  type:  string;
+  userId: string;
+  id: string;
+}
+
+export interface UserContextType {
+  user: User | null;
+  isLoading: boolean;
+  handleLogin: () => void;
+  handleLogout: () => void;
+}
